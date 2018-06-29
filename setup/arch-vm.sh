788be7b0195050fd56df3fea139c3974655df1a3
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-06-28 22:41:06 -0400

    Added basic sgdisk formatting

--- setup/arch-vm.sh
+++ setup/arch-vm.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 EFI_DIR=/sys/firmware/efi/efivars
+DISK=/dev/sda
 
 if [ -z "$(ls -A $EFI_DIR)" ]; then
   echo "Is this on EFI? ($EFI_DIR doesn't exist)"
-  exit 1
+  read -n1 -r -p "Hit any key to continue anyway..."
 else
   echo "EFI system ($EFI_DIR exists!)"
 fi
+
+echo "Writing new GPT"
+# Clear out the existing GPT
+sgdisk -og $DISK
+# Create and configure the EFI partition
+sgdisk -n 2::+100MiB -c 2:"EFI System Partition" -t 2:ef00 $DISK
