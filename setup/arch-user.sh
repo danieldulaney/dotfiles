7d5d65d4219e30a9e8822e87b21fdd467f6a4900
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-07-08 02:52:48 -0400

    Add actual content to arch-user

--- setup/arch-user.sh
+++ setup/arch-user.sh
@@ -5,6 +5,7 @@ USERNAME=daniel
 # Install expected tools
 pacman -S git openssh --noconfirm
 systemctl enable sshd.service
+systemctl start sshd.service
 
 # Create a new user
-useradd "$USERNAME"
+useradd -m "$USERNAME"
