13983aaf767b1bf95d5280d0d5377c7be9fe605b
Daniel Dulaney <dulaney.daniel@gmail.com>
2018-11-06 12:49:15 -0500

    Added recursively cloning the dotfiles repo to the setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env sh
 
+# Make sure the repo was cloned recursively
+(cd ~/.dotfiles && git submodule update --init --recursive)
+
+# Symlink to dotfiles
 ln -s ~/.dotfiles/.zshrc ~
 ln -s ~/.dotfiles/.tmux.conf ~
 ln -s ~/.dotfiles/.vimrc ~
