3da24ec1bf787243e8acf69d61fafa56714b06f0
Daniel Dulaney <dan@dulaney.xyz>
2019-04-09 10:15:17 -0400

    Added xmobar configuration

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,4 +16,5 @@ ln -fs ~/.dotfiles/.gitconfig ~
 ln -fs ~/.dotfiles/.xmonad ~
 ln -fs ~/.dotfiles/.Xresources ~
 ln -fs ~/.dotfiles/.Xresources.d ~
-ln -fs ~/.dotfiles/.fehbg
+ln -fs ~/.dotfiles/.fehbg ~
+ln -fs ~/.dotfiles/.xmobarrc ~
