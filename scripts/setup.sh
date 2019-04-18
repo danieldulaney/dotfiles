abe68992b9dbc625c9ffe48daa87a065c3575b84
Daniel Dulaney <dan@dulaney.xyz>
2019-04-17 23:28:39 -0400

    Added .ghci file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,4 +17,4 @@ ln -fs ~/.dotfiles/.xmonad ~
 ln -fs ~/.dotfiles/.Xresources ~
 ln -fs ~/.dotfiles/.Xresources.d ~
 ln -fs ~/.dotfiles/.fehbg ~
-ln -fs ~/.dotfiles/.xmobarrc ~
+ln -fs ~/.dotfiles/.ghci ~
