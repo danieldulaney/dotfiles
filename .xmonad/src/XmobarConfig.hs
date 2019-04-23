9982ac5b5eca6b0ec520525f38a44c247572424f
Daniel Dulaney <dan@dulaney.xyz>
2019-04-23 19:38:53 -0400

    Custom memory plugin for xmobar

--- .xmonad/src/XmobarConfig.hs
+++ .xmonad/src/XmobarConfig.hs
@@ -3,10 +3,14 @@ module XmobarConfig where
 import Xmobar hiding (main)
 import Solarized
 
+import Control.Monad
+
 import Data.List
 import Data.List.Split hiding (startsWith)
+import qualified Data.Map as M
 import Data.Maybe
 
+import Text.Printf
 import Text.Read
 import Text.Regex.Posix
 
@@ -18,7 +22,23 @@ pad s = " " ++ s ++ " "
 
 highlight = color (bgColor config)
 
-byteUnits = [ ('k', 2^10)
+ok = highlight solarizedGreen
+warning = highlight solarizedYellow
+danger = highlight solarizedRed
+
+alertHighlight :: (Ord a, Fractional a) => a -> (String -> String)
+alertHighlight status
+    | status < 0.7 = id
+    | status < 0.9 = warning
+    | otherwise    = danger
+
+alertHighlightPad :: (Ord a, Fractional a) => a -> (String -> String)
+alertHighlightPad status string = alertHighlight status (lpad ++ string ++ rpad)
+    where (lpad, rpad) = if status < 0.7 then (" ", " ") else ("\x258c", "\x2590")
+
+
+byteUnits = [ (' ', 2^00)
+            , ('k', 2^10)
             , ('M', 2^20)
             , ('G', 2^30)
             , ('T', 2^40)
@@ -31,17 +51,22 @@ checkThat pred item
     | (pred item) = Just item
     | otherwise   = Nothing
 
-fromHuman :: String -> Maybe Integer
-fromHuman s = do
-    { results <- checkThat (\l -> length l == 2) $ regexResults s
-    ; base <- (readMaybe $ head results :: Maybe Integer)
-    ; multiplier <- getUnit $ last results
-    ; return $ base * multiplier
-    } where
-        regexResults :: String -> [String]
-        regexResults s = mrSubList (s =~ "([[:digit:]]+)[[:blank:]]*([[:alpha:]]*)")
-        getUnit :: String -> Maybe Integer
-        getUnit unit = return . snd =<< (listToMaybe $ filter (\(u, _) -> u == head unit) byteUnits)
+withByteUnits :: Float -> String
+withByteUnits v = withUnits (2^10) v ++ "B"
+
+withUnits :: Int -> Float -> String
+withUnits = withUnits' 0
+
+withUnits' :: Int -> Int -> Float -> String
+withUnits' ix mul val
+    | val < 0 = '-' : withUnits' ix mul (-val)
+    | val < (fromIntegral mul) || (ix+1 == length units) = (showNum val) ++ " " ++ units!!ix
+    | otherwise = withUnits' (ix + 1) mul (val / fromIntegral mul)
+    where
+        units = ["", "K", "M", "G", "T", "P", "E", "Z", "Y"]
+        showSingles val = show (round val :: Int)
+        showMults val = printf "%.1g" val :: String
+        showNum = if ix == 0 then showSingles else showMults
 
 startsWith [] _ = True
 startsWith _ [] = False
@@ -49,34 +74,51 @@ startsWith (n:ns) (h:hs)
     | n == h    = startsWith ns hs
     | otherwise = False
 
--- Plugins
-----------
+divideIntegral lhs rhs = (fromIntegral lhs) / (fromIntegral rhs)
+
+maybePrintf1 :: String -> Maybe Double -> String
+maybePrintf1 _ Nothing = "Updating..."
+maybePrintf1 fmt (Just item) = printf fmt item
+
+-- FancyBattery plugin
+----------------------
 
 
 -- FancyMemory plugin
+---------------------
 data FancyMemory = FancyMemory 
     { interval :: Int
     } deriving (Read, Show)
 
 instance Exec FancyMemory where
     alias (FancyMemory _) = "fancymem"
-    run   (FancyMemory _) = show <$> memItem "MemFree"
+    run   (FancyMemory _) = do
+        { memInfo <- parseMem
+        ; let ratio = memInfo !! 0
+        ; let pct = ratio * 100
+        ; let string = printf "%-3.1f%%" pct :: String
+        ; let used = memInfo !! 8
+        ; let total = memInfo !! 3
+        ; return $ icon '\xf538' ++ (alertHighlightPad ratio string) ++ "(" ++ (withByteUnits used) ++ "/" ++ (withByteUnits total) ++ ")"
+        }
     rate  (FancyMemory i) = i
 
 memFile :: IO String
 memFile = readFile "/proc/meminfo"
 
-memItem :: String -> IO (Maybe Integer)
-memItem s = do
+parseMem :: IO [Float]
+parseMem = do 
     { file <- memFile
-    ; let lines = splitLines file
-    ; let matchinglines = filter (startsWith $ s ++ ":") lines
-    ; let matchingvalues = map (getIndex 1 . splitOn ":") matchinglines
-    ; let matchingvalue = listToMaybe matchingvalues >>= fromHuman
-    ; return $ matchingvalue
-    } where
-        splitLines = endBy "\n"
-        getIndex ix list = list!!ix
+    ; let content = map words $ take 8 $ lines file
+    ; let info = M.fromList $ map (\line -> (head line, (read $ line !! 1 :: Float) * 1024)) content
+    ; let [total, free, buffer, cache] = map (info M.!) ["MemTotal:", "MemFree:", "Buffers:", "Cached:"]
+    ; let available = M.findWithDefault (free + buffer + cache) "MemAvailable:" info
+    ; let used = total - available
+    ; let usedratio = used / total
+    ; let freeratio = free / total
+    ; let availableratio = available / total
+    ; return [usedratio, freeratio, availableratio, total, free, buffer, cache, available, used]
+    }
 
 -- Main config
 --------------
@@ -105,7 +147,7 @@ config = defaultConfig
         ]
     , sepChar = "%"
     , alignSep = "}{"
-    , template = "%UnsafeStdinReader% }{ %fancymem% | %memory% ┃ %battery% ┃ %date%"
+    , template = "%UnsafeStdinReader% }{ " ++ "%fancymem% ┃ %battery% ┃ %date%"
     }
 
 main :: IO ()
