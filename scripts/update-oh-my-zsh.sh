af35755fd957b0b2da27bf6f649e2168905540c0
Daniel Dulaney <dan@dulaney.xyz>
2019-05-25 00:22:03 -0400

    Added update-oh-my-zsh script

---
+++ scripts/update-oh-my-zsh.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+root=$(git rev-parse --show-toplevel)
+cd "$root"
+
+echo "Resetting staging area"
+git reset
+
+echo "Ensuring we have the latest .dotfiles repo"
+git pull
+
+pushd oh-my-zsh
+echo "Pulling the newest oh-my-zsh"
+git pull
+popd
+
+git add oh-my-zsh
+git commit -m "Updated oh-my-zsh"
+git push
