66609d69f50ad0f3c67c2705a0a65a6ecb229411
Daniel Dulaney <dan@dulaney.xyz>
2020-03-27 05:15:29 +0000

    Fix update-oh-my-zsh.sh script to use correct paths

--- .scripts/update-oh-my-zsh.sh
+++ .scripts/update-oh-my-zsh.sh
@@ -9,11 +9,14 @@ git reset
 echo "Ensuring we have the latest .dotfiles repo"
 git pull
 
-pushd oh-my-zsh
+pushd .oh-my-zsh
+echo "Explicitly checking out oh-my-zsh master branch"
+git checkout master
+
 echo "Pulling the newest oh-my-zsh"
 git pull
 popd
 
-git add oh-my-zsh
+git add .oh-my-zsh
 git commit -m "Updated oh-my-zsh"
 git push
